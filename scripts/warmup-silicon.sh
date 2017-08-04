693a16261e0bac89ac94f6ef19e5be5ef395e91b
Malte Schwerhoff <malte.schwerhoff@inf.ethz.ch>
2017-08-04 09:39:57 +0200

    Split warm-up tests into separate files

--- scripts/warmup-silicon.sh
+++ scripts/warmup-silicon.sh
@@ -4,6 +4,16 @@ SCRIPT_DIR=${0%/*} # https://stackoverflow.com/a/3588939
 PORT=$1
 
 $SCRIPT_DIR/warmup-viper.sh \
-    --repetitions 2 \
+    --repetitions 1 \
     "$SCRIPT_DIR/silicon.sh --mode nailgun --port $PORT" \
-    $SCRIPT_DIR/warmup.vpr
+    $SCRIPT_DIR/warmup1.vpr
+
+$SCRIPT_DIR/warmup-viper.sh \
+    --repetitions 1 \
+    "$SCRIPT_DIR/silicon.sh --mode nailgun --port $PORT" \
+    $SCRIPT_DIR/warmup2.vpr
+
+$SCRIPT_DIR/warmup-viper.sh \
+    --repetitions 1 \
+    "$SCRIPT_DIR/silicon.sh --mode nailgun --port $PORT" \
+    $SCRIPT_DIR/warmup3.vpr
